bf7c67b0a3e648ddc48111199c1d1d8ab29afdaa
Matthew Ramos <mmramos309@gmail.com>
2020-05-20 14:44:47 -0400

    Added empty project data

---
+++ SuShare/SuShareTests/SuShareTests.swift
@@ -0,0 +1,34 @@
+//
+//  SuShareTests.swift
+//  SuShareTests
+//
+//  Created by Matthew Ramos on 5/20/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import XCTest
+@testable import SuShare
+
+class SuShareTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
