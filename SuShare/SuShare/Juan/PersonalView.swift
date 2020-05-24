78053db0631e017c0cc2130be896248b0a568039
Matthew Ramos <mmramos309@gmail.com>
2020-05-23 23:52:30 -0400

    Added pods, groups, basic storyboard and vcs, xibs

---
+++ SuShare/SuShare/Juan/PersonalView.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalView.swift
+//  SuShare
+//
+//  Created by Matthew Ramos on 5/23/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class PersonalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SuShare/SuShare/Shaniya/CreateSusuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateSusuViewController.swift
+//  SuShare
+//
+//  Created by Matthew Ramos on 5/23/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class CreateSusuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
