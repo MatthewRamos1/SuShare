cc80f7ed2b802af419ab3939fdf153a4f51a374c
Matthew Ramos <mmramos309@gmail.com>
2020-05-23 23:58:16 -0400

    Set tab bar controller in scene delegate

--- SuShare/SuShare/TabController.swift
+++ SuShare/SuShare/TabController.swift
@@ -10,38 +10,17 @@ import UIKit
 
 class TabController: UITabBarController {
     
-    lazy var feedViewController: UIViewController = {
-        let storyboard = UIStoryboard(name: "PostSB", bundle: nil)
-        let vc = storyboard.instantiateInitialViewController()  ?? FeedViewController()
-        vc.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "pencil"), tag: 0)
+    lazy var exploreViewController: UIViewController = {
+        let storyboard = UIStoryboard(name: "ExploreTab", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController() ?? CreateSusuViewController()
+        vc.tabBarItem = UITabBarItem(title: "Explore", image: UIImage(systemName: "gear"), tag: 1)
         return vc
     }()
     
-    lazy var eventViewController: UIViewController = {
-        let storyboard = UIStoryboard(name: "EventSB", bundle: nil)
-        let vc = storyboard.instantiateInitialViewController()  ?? EventViewController()
-        vc.tabBarItem = UITabBarItem(title: "Event", image: UIImage(systemName: "mic"), tag: 1)
-        return vc
-    }()
-    
-    lazy var favoriteViewController: FavoriteViewController = {
-        let vc = FavoriteViewController()
-        vc.tabBarItem = UITabBarItem(title: "Favorite", image: UIImage(systemName: "heart"), tag: 2)
-        return vc
-    }()
-    
-    lazy var profileViewController: ProfileViewController = {
-        let vc = ProfileViewController()
-        vc.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 3)
-        return vc
-    }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewControllers = [feedViewController,
-                           eventViewController,
-                           favoriteViewController,
-                           profileViewController]
+        viewControllers = [exploreViewController]
     }
     
 }
