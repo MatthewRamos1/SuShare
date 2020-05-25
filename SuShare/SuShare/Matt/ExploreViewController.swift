c4f020b0ddf703988bdcc4be500e9f6c6c8adb46
Matthew Ramos <mmramos309@gmail.com>
2020-05-25 12:44:34 -0400

    Set up storyboard constraints

--- SuShare/SuShare/Matt/ExploreViewController.swift
+++ SuShare/SuShare/Matt/ExploreViewController.swift
@@ -10,21 +10,14 @@ import UIKit
 
 class ExploreViewController: UIViewController {
 
+    @IBAction func exploreSegementedControl(_ sender: UISegmentedControl) {
+    }
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
