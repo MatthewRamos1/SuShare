c5861c6b052e25a26d666edd4daa0b11ab74a9d5
Jaheed <jaheedhaynes@pursuit.org>
2020-06-10 14:07:00 -0400

    added side view logic. Added controllers for Friends & User Settings

---
+++ SuShare/SuShare/Jaheed/SideView/Friends/UserFriendsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UserFriendsViewController.swift
+//  SuShare
+//
+//  Created by Jaheed Haynes on 6/10/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class UserFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
---
+++ SuShare/SuShare/Jaheed/SideView/Settings/SettingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsViewController.swift
+//  SuShare
+//
+//  Created by Jaheed Haynes on 6/8/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+enum SettingsType{
+    case username
+    case paymentOption
+    case signOut
+}
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
