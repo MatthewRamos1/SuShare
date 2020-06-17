0a7baf7c1c74a5cf679db35db23277fa2aacb745
Matthew Ramos <mmramos309@gmail.com>
2020-06-17 01:52:08 -0400

    Setting up custom view with custom button, began stripe payment context implementation

--- SuShare/SuShare/Matt/PaymentViewController.swift
+++ SuShare/SuShare/Matt/PaymentViewController.swift
@@ -7,20 +7,60 @@
 //
 
 import UIKit
+import Stripe
 
 class PaymentViewController: UIViewController {
     
+    private var paymentContext = STPPaymentContext()
     public var suShare: SuShare?
+    private var paymentView = PaymentView()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Checkout"
-        view.backgroundColor = .systemGreen
+        paymentView.backgroundColor = .white
+        view = paymentView
+        
     }
    
 }
 
 class PaymentView: UIView {
     
+    
+    private lazy var joinSuShareButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.text = "Subscribe"
+        button.backgroundColor = #colorLiteral(red: 0, green: 0.6613236666, blue: 0.617059052, alpha: 1)
+        button.titleLabel?.textColor = .white
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        addSubview(joinSuShareButton)
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            joinSuShareButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            joinSuShareButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            joinSuShareButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            joinSuShareButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+            
+    }
 }
