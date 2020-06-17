41ee256e27645305c077dc8a276f1f8f244e9ede
uwagboe <sm.uwagboe@gmail.com>
2020-06-17 15:11:15 -0400

    added a test to check storage to make sure files are being uploaded properly

--- SuShare/SuShareTests/SuShareTests.swift
+++ SuShare/SuShareTests/SuShareTests.swift
@@ -7,16 +7,55 @@
 //
 
 import XCTest
+import FirebaseStorage
+
 @testable import SuShare
 
 class SuShareTests: XCTestCase {
 
    //testing storage image
     func testUploadImageToStorage(){
+       
+        let exp = XCTestExpectation(description: "suShare photo upload")
+        let queryPath = Bundle.main.path(forResource: "createSushare-wPlus", ofType: "png")
         
-        let exp 
+        guard let path = queryPath else {
+            XCTFail("could not access the path")
+            return
+        }
         
+        //act
+        guard let jpgData = FileManager.default.contents(atPath: path) else {
+            XCTFail("failed because contents inside of path is nil")
+            return
+        }
+        
+        let storageRef = FirebaseStorage.Storage.storage().reference()
+        let photoRef = storageRef.child("Photo/appLogo")
+        
+        let metaData = StorageMetadata()
+               metaData.contentType = "photo/png"
+        
+        
+        // act
+               photoRef.putData(jpgData, metadata: metaData) {
+                   (metaData, error) in
+                   exp.fulfill()
+                   
+                   // assert
+                   if let error = error {
+                       XCTFail("failure to upload video \(error.localizedDescription)")
+                   }
+                   
+                   XCTAssert(true)
+               }
+                       
+               wait(for: [exp], timeout: 10)
+                 
+           }
+    
+    
     }
     
 
-}
+
