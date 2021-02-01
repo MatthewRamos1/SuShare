1bf094c2335aa48759bd315720b817e2d8fb1699
Juan C <juanceballos@pursuit.org>
2021-01-31 21:16:18 -0500

    loading alert for changing profile pic

--- SuShare/SuShare/Extensions/UIViewController + Alerts.swift
+++ SuShare/SuShare/Extensions/UIViewController + Alerts.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 extension UIViewController {
+    
     func showAlert(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alertController.addAction(okAction)
         present(alertController, animated: true, completion: nil)
+    }
+    
+    func showUploadAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(alertController, animated: true, completion: nil)
+        
     }
     
     func confirmingShowAlert(title: String) {
--- SuShare/SuShare/FirebaseServices/StorageService.swift
+++ SuShare/SuShare/FirebaseServices/StorageService.swift
@@ -13,9 +13,10 @@ class StorageService {
   
   // reference to the Firebase storage
   private let storageRef = Storage.storage().reference()
+  let view = LoadingView()
   
   // default parameters in Swift e.g userId: String? = nil
-  public func uploadPhoto(userId: String? = nil, sushareId: String? = nil, image: UIImage, completion: @escaping (Result<URL, Error>) -> ()) {
+    public func uploadPhoto(userId: String? = nil, sushareId: String? = nil, image: UIImage, vc: UIViewController? = nil, completion: @escaping (Result<URL, Error>) -> ()) {
     
     // convert UIImage to Data because this is the object posting to Firebase Storage
     guard let imageData = image.jpegData(compressionQuality: 1.0) else {
@@ -34,7 +35,7 @@ class StorageService {
     let metadata = StorageMetadata()
     metadata.contentType = "image/jpg" // MIME type
     
-    let _ = photoReference.putData(imageData, metadata: metadata) { (metadata, error) in
+    let uploadTask = photoReference.putData(imageData, metadata: metadata) { (metadata, error) in
       if let error = error {
         completion(.failure(error))
       } else if let _ = metadata {
@@ -47,5 +48,22 @@ class StorageService {
         }
       }
     }
+    
+    uploadTask.observe(.progress) { snapshot in
+        let percentComplete = 100 * Double(snapshot.progress!.completedUnitCount) / Double(snapshot.progress!.totalUnitCount)
+        print(percentComplete)
+        DispatchQueue.main.async {
+            vc?.showUploadAlert(title: "Uploading", message: "Please Wait...")
+        }
+    }
+    
+    uploadTask.observe(.success) { snapshot in
+        print("success")
+        DispatchQueue.main.async {
+            vc?.dismiss(animated: true, completion: nil)
+            vc?.showAlert(title: "Success", message: "your profile has been updated")
+        }
+    }
+    
   }
 }
